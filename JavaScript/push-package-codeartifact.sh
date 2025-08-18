c8a5c0334003d91e85145b75e619fd8a13661ea1
Grace <gquinn7@outlook.com>
2025-08-18 22:04:07 +0100

    Tidy up Bash scripts

--- JavaScript/push-package-codeartifact.sh
+++ JavaScript/push-package-codeartifact.sh
@@ -10,11 +10,11 @@ aws codeartifact login \
   --repository $CODE_ARTIFACT_PACKAGE_REPOSITORY
 
 # 3. Download .tgz locally 
-npm pack axios-retry@4.5.0 --registry https://registry.npmjs.org/
+npm pack axios@4.5.0 --registry https://registry.npmjs.org/
 
 # 4. Extract .tgz into tmp file 
-# May need to disable some script stages in package.json, e.g. prepare for axios-retry
-mkdir tmp && tar -xzf axios-retry-4.5.0.tgz -C tmp && cd tmp/package
+# May need to disable some script stages in package.json, e.g. prepare for axios
+mkdir tmp && tar -xzf axios-4.5.0.tgz -C tmp && cd tmp/package
 vim package.json # remove prepare script
   
 npm publish --ignore-scripts --registry=$_CODE_ARTIFACT_REPOSITORY
